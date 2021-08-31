998f5816095e4656cd16d1b27742f5b8369d8331
Naimur53 <naimurrhman53@gmail.com>
2021-08-31 14:43:00 +0600

    compelete

--- elements/js/main.js
+++ elements/js/main.js
@@ -1,4 +1,4 @@
-var para = " ";
+var para = " qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiopqwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuioqwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuioqwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiopqwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiopqwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yuiop qwert yui";
 var sp = para.split(""); 
 var result ,count=0;
 for(var i=0; i<sp.length; i++){
